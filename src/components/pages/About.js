94204a2d8fda06228e975f7314aa92da0765065e
Utkan Bolat <utkan1994@hotmail.com>
2020-07-02 02:19:31 +0300

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+ function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>This is an About page</p>
+        </React.Fragment>
+
+    )
+}
+
+export default About;
